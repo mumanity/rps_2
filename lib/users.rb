e53bdb7bac75aeb71fff7f3b66ec25fcc8208e32
Emily Schomer <minischomer@gmail.com>
2014-05-19 11:09:42 -0500

    updating user class

--- lib/users.rb
+++ lib/users.rb
@@ -2,4 +2,9 @@ class RPS::Users
 
   attr_reader :name, :password
 
-  def initialize(name, password)
+  def initialize(data)
+    @username = data[:name]
+    @password = data[:password]
+  end
+
+end
