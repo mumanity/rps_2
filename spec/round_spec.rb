3b8e4a0e86c22bcc1eda2899e822cf001ea87cd7
Emily Schomer <minischomer@gmail.com>
2014-05-19 15:01:38 -0500

    adding working test for rounds class

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -3,22 +3,21 @@ require 'pry'
 
 describe 'Round' do
    before(:each) do
-    @players = RPS::Round.new({:move! => 'rock', :move2 => 'paper'})
+    @players = RPS::Round.new('player1', 'player2', 'move1', 'move2', 'winner', 1)
   end
 
   it 'exitst' do
-    expect(RPS::Game).to be_a(Class)
+    expect(RPS::Round).to be_a(Class)
   end
 
-  it '' do
+  it 'accessors work' do
 
-    expect(@players.player1).to eq('')
-    expect(@players.player2).to eq('')
-  end
-
-  it '' do
-    expect(@players.player1_wins).to eq(0)
-    expect(@players.player2_wins).to eq(0)
+    expect(@players.player1).to eq('player1')
+    expect(@players.player2).to eq('player2')
+    expect(@players.move1).to eq('move1')
+    expect(@players.move2).to eq('move2')
+    expect(@players.winner).to eq('winner')
+    expect(@players.round_id).to eq(1)
   end
 
 end
