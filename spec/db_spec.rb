3c66dd90655a8705b7cc3893bc0f9e64d5a5ba2c
Emily Schomer <minischomer@gmail.com>
2014-05-20 12:01:25 -0500

    working rspec for users methods

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -7,17 +7,23 @@ describe 'RPS::DB' do
     @db = RPS::DB.new
 
     @this = @db.create_user({:password => 'winning'})
+    @that = @db.create_user({:password => 'losing'})
+    @those = @db.create_user({:password => 'observing'})
   end
 
   it 'exitst' do
     expect(RPS::DB).to be_a(Class)
   end
 
-  xit 'creates a user with a id' do
-    expect(@lady.id).to eq('Sheila')
+  it 'creates a user with a unique id' do
+    expect(@this.id).to eq(1)
+    expect(@that.id).to eq(2)
+    expect(@those.id).to eq(3)
   end
 
-  xit 'creates a user with a password' do
-    expect(@lady.password).to eq('winning')
+  it 'creates a user with a password' do
+    expect(@this.password).to eq('winning')
+    expect(@that.password).to eq('losing')
+    expect(@those.password).to eq('observing')
   end
 end
