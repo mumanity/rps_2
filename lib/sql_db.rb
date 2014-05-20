743723ceb122bd383c36416db7e899dc796cd051
Emily Schomer <minischomer@gmail.com>
2014-05-20 10:43:28 -0500

    adding template info / examples to db

--- lib/sql_db.rb
+++ lib/sql_db.rb
@@ -1,5 +1,85 @@
 class RPS::DB
 
+# USER
+  def create_user(data)
+  # #attrs = { name: "this" }
+  #   @project_count += 1
+  #   attrs[:id] = @project_count
+  #   attrs[:completed] = false
+  #   #attrs = { name: "this", :id => 0, :competed => false }
+  #   @projects[attrs[:id]] = attrs
+  #   RPS::Users.new((data[:id], data[:name], data[:completed])
+  end
+
+  def get_user(id)
+    # project = @projects[id]
+    # build_project(project)  end
+
+  def update_user(id, data)
+    # @projects[id].merge!(data)
+  end
+
+  def build_user(data)
+    # Project.new(data[:id], data[:name], data[:completed])
+  end
+
+# GAMES
+  def create_game
+  # #attrs = { name: "this" }
+  #   @project_count += 1
+  #   attrs[:id] = @project_count
+  #   attrs[:completed] = false
+  #   #attrs = { name: "this", :id => 0, :competed => false }
+  #   @projects[attrs[:id]] = attrs
+  #   RPS::Users.new((data[:id], data[:name], data[:completed])
+  end
+
+  def get_game(id)
+    # project = @projects[id]
+    # build_project(project)
+  end
+
+  def update_game(id, data)
+    # @projects[id].merge!(data)
+  end
+
+  def build_game(data)
+    # Project.new(data[:id], data[:name], data[:completed])
+  end
+
+  def play_game
+# starts game
+  end
+
+# ROUND
+  def create_round(data)
+  # #attrs = { name: "this" }
+  #   @project_count += 1
+  #   attrs[:id] = @project_count
+  #   attrs[:completed] = false
+  #   #attrs = { name: "this", :id => 0, :competed => false }
+  #   @projects[attrs[:id]] = attrs
+  #   RPS::Users.new((data[:id], data[:name], data[:completed])
+  end
+
+  def get_round
+    # project = @projects[id]
+    # build_project(project)
+  end
+
+  def update_round(id, data)
+    # @projects[id].merge!(data)
+  end
+
+  def build_round(data)
+    # Project.new(data[:id], data[:name], data[:completed])
+  end
+
+end
+
+
+
+
  # def initialize
  #    @db = SQLite3::Database.new "test.db"
 
@@ -11,10 +91,3 @@ class RPS::DB
  #        password string,
  #        );
  #    SQL
-
-end
-
-# C
-# R
-# U
-# D
