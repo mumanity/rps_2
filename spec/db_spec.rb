71e5a8ce7c70d81f78c5e088084f218122e73bc8
Emily Schomer <minischomer@gmail.com>
2014-05-21 09:03:48 -0500

    adding structure for games / rounds testing

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'pry'
 
-describe 'RPS::DB' do
+describe 'RPS::DB / Users' do
    before(:each) do
 
     @db = RPS::DB.new
@@ -22,6 +22,49 @@ describe 'RPS::DB' do
   end
 
   it 'creates a user with a password' do
+    expect(@this.password).to eq('winning')
+    expect(@that.password).to eq('losing')
+    expect(@those.password).to eq('observing')
+  end
+end
+
+describe 'RPS::DB / Games' do
+   before(:each) do
+    @db = RPS::DB.new
+    @this = @db.create_game({:p1_id => 1, :p2_id => 2})
+  end
+
+  xit 'creates a user with a unique id' do
+    expect(@this.id).to eq(1)
+    expect(@that.id).to eq(2)
+    expect(@those.id).to eq(3)
+  end
+
+  xit 'creates a user with a password' do
+    expect(@this.password).to eq('winning')
+    expect(@that.password).to eq('losing')
+    expect(@those.password).to eq('observing')
+  end
+end
+
+
+describe 'RPS::DB / Rounds' do
+   before(:each) do
+
+    @db = RPS::DB.new
+
+    @this = @db.create_round({:password => 'winning'})
+    @that = @db.create_round({:password => 'losing'})
+    @those = @db.create_round({:password => 'observing'})
+  end
+
+  xit 'creates a user with a unique id' do
+    expect(@this.id).to eq(1)
+    expect(@that.id).to eq(2)
+    expect(@those.id).to eq(3)
+  end
+
+  xit 'creates a user with a password' do
     expect(@this.password).to eq('winning')
     expect(@that.password).to eq('losing')
     expect(@those.password).to eq('observing')
