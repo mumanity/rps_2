f525011f895f1885be587024efd067641adcc39f
Emily Schomer <minischomer@gmail.com>
2014-05-21 10:52:27 -0500

    adding first run through sign in command

---
+++ lib/commands/sign_in.rb
@@ -0,0 +1,12 @@
+require_relative '../rps.rb'
+
+class RPS::SignIn
+
+  def run(input)
+    response = RPS::SQLDB.get_user_by_name(data[:id])
+    if response[0].last == data[:password]
+      return {:success? => true, :message => 'login successful!'}
+    else
+      return {:success? => false, :message => 'incorrect password'}
+  end
+end
